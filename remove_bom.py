2320129d53a3cba519234fc6df05abf9de6ff1a4
ronald1971-debug <ronaldvervoort1971@gmail.com>
2026-06-13 18:18:44 -0500

    Clean up temporary test scripts and results files

--- remove_bom.py
+++
@@ -1,8 +0,0 @@
-# Remove BOM from compose.yaml
-with open('compose.yaml', 'r', encoding='utf-8-sig') as f:
-    content = f.read()
-
-with open('compose.yaml', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print('BOM removed and file saved with UTF-8 encoding')
--- remove_bom_clean.py
+++
@@ -1,7 +0,0 @@
-with open('compose.yaml', 'r', encoding='utf-8-sig') as f:
-    content = f.read()
-
-with open('compose_new.yaml', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print('Created compose_new.yaml without BOM')
